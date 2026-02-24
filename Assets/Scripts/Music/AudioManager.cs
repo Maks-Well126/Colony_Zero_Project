851b7ccaf06f7d5ae02b808a13859b4143b9fd16
Alexsey comp 2 <alexrarder80740@gmail.com>
2026-02-24 14:34:45 +0300

    Add UI SFX and logic(set it up)

---
+++ Assets/Scripts/Music/AudioManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("UI Sounds")]
+    [SerializeField] private AudioClip buttonHoverSound;
+    [SerializeField] private AudioClip buttonClickSound;
+    [SerializeField] private AudioClip sliderSound;
+    [SerializeField] private AudioClip popupOpenSound;
+    [SerializeField] private AudioClip popupCloseSound;
+    [SerializeField] private AudioClip errorSound;
+
+    [Header("Settings")]
+    [SerializeField][Range(0f, 1f)] private float sfxVolume = 1f;
+    [SerializeField][Range(0f, 1f)] private float musicVolume = 0.5f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (musicSource) musicSource.volume = musicVolume;
+        if (sfxSource) sfxSource.volume = sfxVolume;
+    }
+
+    public void PlayButtonHover() => PlaySFX(buttonHoverSound);
+    public void PlayButtonClick() => PlaySFX(buttonClickSound);
+    public void PlaySliderSound() => PlaySFX(sliderSound);
+    public void PlayPopupOpen() => PlaySFX(popupOpenSound);
+    public void PlayPopupClose() => PlaySFX(popupCloseSound);
+    public void PlayError() => PlaySFX(errorSound);
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip != null && sfxSource != null)
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayMusic(AudioClip musicClip, bool loop = true)
+    {
+        if (musicSource != null && musicClip != null)
+        {
+            musicSource.clip = musicClip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+    }
+
+    public void StopMusic() => musicSource?.Stop();
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        if (musicSource) musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        if (sfxSource) sfxSource.volume = volume;
+    }
+}
