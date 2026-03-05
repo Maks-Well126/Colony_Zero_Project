2c1ac5705d7408c6dc69de8319d8ec54825be556
Maksim Korotenko <mskorotenko@mail.ru>
2026-03-05 22:35:54 +0300

    Obstacle

--- Assets/Scripts/Music/AudioManager.cs
+++ Assets/Scripts/Music/AudioManager.cs
@@ -28,7 +28,7 @@ public class AudioManager : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField][Range(0f, 1f)] private float sfxVolume = 1f;
-    [SerializeField][Range(0f, 1f)] private float musicVolume = 0.5f;
+    [SerializeField][Range(0f, 1f)] private float musicVolume = 0.05f;
     [SerializeField] private float fadeTime = 2f;
 
     private AudioSource currentSource;
